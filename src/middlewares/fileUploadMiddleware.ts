2d01ae47aa00288567f7c8209e0a6131d26ba647
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2023-07-31 22:35:25 +0300

    feat: Implement Multer for File Upload
    In this commit, Multer, a middleware for handling file uploads, has been integrated into the application. Multer is used to process and store files uploaded by users. The configuration for Multer is as follows:
    
    1. storage: The destination and filename for uploaded files are specified in the diskStorage object. The destination property determines the directory where the uploaded files will be stored, which is set to the ./media directory in this case. The filename function generates a unique filename for each uploaded file by appending the current timestamp (obtained from +new Date()) to the original filename.
    
    2. upload: The Multer middleware is created using the multer function, and the storage configuration is passed as an argument. This middleware can be used to handle file uploads in routes or endpoints where file upload is required.
    
    With Multer integrated into the application, users can now upload files to the specified ./media directory, and each file will be assigned a unique filename based on the current timestamp. This allows the application to handle file uploads efficiently and securely.

---
+++ src/middlewares/fileUploadMiddleware.ts
@@ -0,0 +1,12 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: "./media",
+  filename: (req, file, cb) => {
+    cb(null, `${+new Date()}${file.originalname}`);
+  },
+});
+
+export const upload = multer({
+  storage,
+});
